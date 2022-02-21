6110dc80b57cc05f1316ba56a129dbe8d0c14aca
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-21 12:28:39 -0800

    removed old readme

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,9 +1,11 @@
 <template>
+<div>
   <nav>
     <router-link to="/">Home</router-link>
     <router-link to="/games">Games</router-link>
     <router-link to="/about">About</router-link>
   </nav>
+</div>
 </template>
 
 <script>
